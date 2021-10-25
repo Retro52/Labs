dd755dbc68ee64a2c608a8f1f9d61d0297deae37
Retro52 <antonov.ukraine@gmail.com>
2021-10-25 11:14:53 +0300

    Step Seven: 127 hours

--- Labs/Java labs/Lab 2/src/main/Java/oop/lab2/Main.java
+++ Labs/Java labs/Lab 2/src/main/Java/oop/lab2/Main.java
@@ -8,26 +8,35 @@ class StringCalculator
 {
     public static int Add(String numbers) throws IllegalArgumentException
     {
-        char delimiter = ',';
+        String delimiter = ",";
         if (numbers.startsWith("//"))
         {
-            delimiter = numbers.charAt(2);
-            Pattern pattern = Pattern.compile("//.\n");
-            Matcher matcher = pattern.matcher(numbers);
-            while (matcher.find())
+            if (numbers.charAt(2) == '[')
             {
-                numbers = numbers.replace(numbers.substring(matcher.start(), matcher.end()), "");
+                Pattern pattern = Pattern.compile("//\\[.+\n");
+                Matcher matcher = pattern.matcher(numbers);
+                while (matcher.find())
+                {
+                    delimiter = numbers.substring(matcher.start(), matcher.end());
+                    numbers = numbers.replace(delimiter, "");
+                    delimiter = delimiter.replace("//[", "").replace("]\n", "");
+                }
+            }
+            else
+            {
+                delimiter = "" + numbers.charAt(2);
+                numbers = numbers.replace("//" + delimiter + "\n", "");
             }
         }
         if (numbers.length() == 0)
         {
             return 0;
         }
-
         else
         {
 
             int result = 0;
+            delimiter = ProceedDelimiter(delimiter);
             List <String> SplitComas = Arrays.stream(numbers.split(String.valueOf(delimiter))).toList();
             List <String> Final = new Vector<>();
             List<Integer> negatives = new Vector<>();
@@ -59,6 +68,23 @@ class StringCalculator
         }
     }
 
+    private static String ProceedDelimiter(String delimiter)
+    {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < delimiter.length(); i++)
+        {
+            char c = delimiter.charAt(i);
+            if (c == '+' || c == '*' || c == '?' || c == '|' || c == '^' || c == '$')
+            {
+                result.append("\\").append(delimiter.charAt(i));
+            }
+            else
+            {
+                result.append(delimiter.charAt(i));
+            }
+        }
+        return result.toString();
+    }
     private static String formErrorMessage(List<Integer> negatives)
     {
         StringBuilder message = new StringBuilder("Negatives not allowed [");
